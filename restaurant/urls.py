e3dee332137f7e666d9e1e3be678d139c3776413
Andriy <andriykrav@gmail.com>
2023-04-01 17:50:54 +0300

    add views.py and modify urls.py, restaurant/urls.py and settings.py

---
+++ restaurant/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import index, CookListView, CookDetailView
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("cooks/", CookListView.as_view(), name="cook-list",),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+]
+
+app_name = "restaurant"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views import View, generic
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import DishType, Dish, Cook
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "restaurant/index.html", context=context)
+
+
+class CookListView(LoginRequiredMixin,generic.ListView):
+    model = Cook
+    context_object_name = 'cooks'
+    template_name = 'cook_list.html'
+    paginate_by = 5
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+    context_object_name = 'cook'
+    template_name = 'cook_detail.html'
+    success_url = reverse_lazy("taxi:driver-list")
