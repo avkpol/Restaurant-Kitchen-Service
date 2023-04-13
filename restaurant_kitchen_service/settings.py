51f8ecab674dff908694133bd4857ef683b36738
Andriy <andriykrav@gmail.com>
2023-04-13 11:07:03 +0300

    remove insecure Django secret key from settings.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/hpythonowto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned giton in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
@@ -82,26 +82,26 @@ WSGI_APPLICATION = "restaurant_kitchen_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ndqwmvjw',
-        'USER': 'ndqwmvjw',
-        'PASSWORD': '5azesLq-r-cFZ1G8T6B9oqI_qHzS9U2u',
-        'HOST': 'hattie.db.elephantsql.com',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ndqwmvjw',
+#         'USER': 'ndqwmvjw',
+#         'PASSWORD': '5azesLq-r-cFZ1G8T6B9oqI_qHzS9U2u',
+#         'HOST': 'hattie.db.elephantsql.com',
+#         'PORT': '5432',
+#     }
+# }
 
-# postgres://ndqwmvjw:5azesLq-r-cFZ1G8T6B9oqI_qHzS9U2u@hattie.db.elephantsql.com/ndqwmvjw
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES["default"].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
